a22e3b69b15eaf1e48d7c1d9431cb56aa9332e48
Kuba Birecki <kuba.birecki@crystalplanet.io>
2017-10-01 11:31:26 +0200

    Add post components

--- src/cljs/andromeda/components.cljs
+++ src/cljs/andromeda/components.cljs
@@ -1,7 +1,7 @@
 (ns andromeda.components
   (:require [reagent.core :as reagent :refer [atom]]
             [andromeda.config :as config]
-            [andromeda.utils :refer [className]]))
+            [andromeda.utils :refer [className date]]))
 
 (defn fa [icon]
   "Font awesome icons."
@@ -69,6 +69,25 @@
     [:img.sidebar-image__img {:src src
                               :alt description}]])
 
+(defn sidebar-greeting
+  "The greeting message that appears in the sidebar."
+  []
+  [:div.sidebar-greeting
+    [:div.sidebar-greeting__hi "Hi!"]
+    "Placeholder"])
+
+(defn sidebar-notes
+  "The notes at the bottom of the sidebar."
+  []
+  [:div.sidebar-notes
+    [:div.sidebar-notes__item
+      [fa "wordpress"]
+      "Proudly published with WordPress"]
+    [:div.sidebar-notes__item
+      [fa "copyright"]
+      "2017, Kuba Birecki"]
+    ])
+
 (defn sidebar-text
   "A text box component to be used within the sidebar."
   []
@@ -85,25 +104,120 @@
 
 (defn share-button
   "Share buttons for social media."
-  ([icon url] (share-button icon url false))
-  ([icon url is-small]
+  ([icon url] (share-button icon nil url))
+  ([icon action url]
     [:a.share-button
-      {:class (className {"is-small" is-small
+      {:class (className {"is-small" (not action)
                           icon true})
        :href url}
-      [fa icon]]))
+      [fa icon]
+      (when action [:span.share-button__action action])]))
+
+(defn sidebar-layout
+  "Page content layout with a sidebar."
+  [& children]
+  [:div.sidebar-layout
+    [:div.sidebar-layout__content
+      (map-indexed (fn [idx elem] (with-meta elem {:key idx}))
+                   children)]
+    [components/sidebar
+      [:div.sidebar-layout__section.sidebar-layout__social-links
+        (map #(with-meta [components/social-button (:icon %) (:link %)] {:key (:icon %)})
+             config/social-links)]
+      [:div.sidebar-layout__section.sidebar-layout__about
+        [components/sidebar-greeting]
+        [components/sidebar-image "/img/tmp.jpg"
+                                  "That's me!"]
+        [components/sidebar-text]]
+      [:div.sidebar-layout__section
+        [components/search-input]]
+      [:div.sidebar-layout__section
+        [components/sidebar-notes]]]])
+
+(defn disqus-thread
+  "Loads a Disqus comments thread."
+  [id title path]
+  (let [shortname "besidesprogramming-dev"
+        website-url "https://besidesprogramming.com"
+        load-disqus #(if (.-DISQUS js/window)
+                         (.reset (.-DISQUS js/window) #js {:reload true})
+                         (.appendChild (or (.-head js/document) (.-body js/document))
+                                       (doto (.createElement js/document "script")
+                                             (.setAttribute "async" true)
+                                             (.setAttribute "src" (str "//" shortname ".disqus.com/embed.js")))))]
+    (reagent/create-class
+      {:component-did-mount #(load-disqus)
+
+       :component-did-update #(load-disqus)
+
+       :display-name "disqus-thread"
+
+       :reagent-render (fn [id title path]
+                         (set! (.-disqus_shortname js/window) shortname)
+                         (set! (.-disqus_identifier js/window) id)
+                         (set! (.-disqus_title js/window) title)
+                         (set! (.-disqus_url js/window) (str website-url "/" path))
+
+                         [:div#disqus_thread.disqus-thread])})))
+
+(defn post-action-button
+  "Action button for posts."
+  [action url]
+  [:a.post-action-button {:href url} action])
 
 (defn page-title
   "Page title header."
-  [text]
-  [:h2.page-title text])
+  ([title]
+    [:h2.page-title title])
+  ([title href]
+    [:h2.page-title
+      [:a.page-title__link
+        {:href href}
+        title]]))
 
 (defn post-header
-  "Post header including social links & comment count."
+  "Post header including share buttons & date."
   [post]
   [:div.post-header
-    [page-title "Nanana nanana nanana batman!"]
+    [page-title (:title post) (str "/" (:slug post))]
     [:div.post-header__meta
       (share-button "facebook" "#")
       (share-button "twitter" "#")
-      (share-button "pinterest" "#")]])
+      (share-button "pinterest" "#")
+      [:span.post-header__date (date (:date post))]
+      ]])
+
+(defn post-content
+  "Post content container."
+  [content]
+  [:div.post-content {:dangerouslySetInnerHTML {:__html content}}])
+
+(defn post-footer
+  "Post footer including share, next and previous buttons."
+  [post]
+  [:div.post-footer
+    [:div.post-footer__share
+      (share-button "facebook" "Share" "#")
+      (share-button "twitter" "Tweet" "#")
+      (share-button "pinterest" "Pin" "#")]
+    [:div.post-footer__links
+      [:a.post-footer__link.is-prev
+        [:span.post-footer__link-label "previous"]
+        [:span.post-footer__link-title "I wrote that one last week."]]
+      [:a.post-footer__link.is-next
+        [:span.post-footer__link-label "next"]
+        [:span.post-footer__link-title "And this one I will write in the future."]]]])
+
+(defn post-excerpt
+  "Post excerpt block."
+  ([post] (post-excerpt post false))
+  ([post full-content]
+    (let [content (if full-content :content :excerpt)]
+      [:div.post-excerpt
+        [post-header post]
+        [post-content (content post)]
+        [:div.post-excerpt__actions
+          (if full-content
+            [post-action-button [:span [fa "comment"] "Join discussion"]
+                                (str "/" (:slug post) "#disqus_thread")]
+            [post-action-button "Read more…" (str "/" (:slug post))])]])))
