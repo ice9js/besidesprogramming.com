70ccbc1da9e96f226d900dddd80c4107183e197d
Kuba Birecki <kuba.birecki@crystalplanet.io>
2018-10-21 19:07:25 +0200

    Move api calls into a separate apiRequest function

--- client/data/posts/index.js
+++ client/data/posts/index.js
@@ -1,19 +1,9 @@
 /**
  * Internal dependencies
  */
-import { queryParams } from 'data/utils';
+import { apiRequest } from 'data/utils';
 
 /**
  * Dispatches a request for posts to the server
  */
-export const fetchPosts = (	params, onSuccess, onError ) =>
-	fetch( `https://besidesprogramming.com/wp-json/wp/v2/posts?${ queryParams( params ) }` )
-		.then( response => {
-			response.json()
-				.then( ( data ) => onSuccess( {
-					headers: response.headers,
-					status: response.status,
-					data,
-				} ) );
-		} )
-		.catch( onError );
+export const fetchPosts = ( params, onSuccess, onError ) => apiRequest( '/posts', params, onSuccess, onError );
