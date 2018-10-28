d8467fbd9f623cc77dfdab3498eda2e2bae3db9b
Kuba Birecki <kuba.birecki@crystalplanet.io>
2018-10-28 21:33:40 +0100

    Remove Redux dependency

--- client/index.jsx
+++ client/index.jsx
@@ -2,7 +2,6 @@
  * External dependencies
  */
 import React from 'react';
-import { Provider } from 'react-redux';
 import { BrowserRouter } from 'react-router-dom';
 import { render } from 'react-dom';
 
@@ -10,7 +9,6 @@ import { render } from 'react-dom';
  * Internal dependencies
  */
 import App from 'components/app';
-import store from 'state';
 
 render(
 	<BrowserRouter>
