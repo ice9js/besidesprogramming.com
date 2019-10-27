3726acca1db788226a10cee18d907eeaa6d0735a
Kuba Birecki <kuba.birecki@crystalplanet.io>
2019-10-27 17:41:59 +0100

    Add suport for code highlighting

--- client/components/post-content/index.jsx
+++ client/components/post-content/index.jsx
@@ -1,15 +1,30 @@
 /**
  * External dependencies
  */
-import React from 'react';
+import React, { useEffect, useRef } from 'react';
 import PropTypes from 'prop-types';
+import { highlightBlock } from 'highlightjs';
+import { forEach } from 'lodash';
 
-const PostContent = ( { content } ) => (
-	<div
-		className="post-content"
-		dangerouslySetInnerHTML={ { __html: content } }>
-	</div>
-);
+const PostContent = ( { content } ) => {
+	const ref = useRef();
+
+	useEffect( () => {
+		if ( typeof window === 'undefined' || ! ref.current ) {
+			return;
+		}
+
+		forEach( ref.current.querySelectorAll( 'pre code' ), highlightBlock );
+	}, [ content ] );
+
+	return (
+		<div
+			ref={ ref }
+			className="post-content"
+			dangerouslySetInnerHTML={ { __html: content } }>
+		</div>
+	);
+}
 
 PostContent.propTypes = {
 	content: PropTypes.string.isRequired,
